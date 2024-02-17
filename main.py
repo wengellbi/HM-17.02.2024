c03a2b0a39e70622210dcf31bb202304cd79efec
Ibrahim <ibrahim.h.ovale@gmail.com>
2024-02-17 12:25:01 +0300

    HM 17.02.2024

---
+++ main.py
@@ -0,0 +1,24 @@
+class Google:
+    name = "Google"
+    function = "SearchingSystem"
+
+    def searching(self):
+        pass
+
+class Netflix(Google):
+    def __int__(self, name, function):
+        self.name = name
+        self.function = function
+
+        def watching_videos(self):
+            pass
+
+class Netflix_Video("Netflix_Video"):
+    def __int__(self, name, function):
+        self.name = name
+        self.function = function
+
+        def watching_film(self):
+            pass
+
+Netflix_Video = Netflix_Video("Netflix_Video", "function""show a film")
